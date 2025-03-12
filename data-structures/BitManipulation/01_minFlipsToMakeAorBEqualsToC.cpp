f3b698d971d62a60f2b7bc5cc6a3fa2a0b9caed5
Rishi Srivastava <rishisrivastava.ju@gmail.com>
2025-03-12 13:39:33 +0530

    Lec 01 : Minimum Bits Required to flip to Make A or B equal to C

--- data-structures/BitManipulation/01_minFlipsToMakeAorBEqualsToC.cpp
+++ data-structures/BitManipulation/01_minFlipsToMakeAorBEqualsToC.cpp
@@ -1,25 +1,47 @@
-class Solution {
-    public:
-        int minFlips(int a, int b, int c) {
-            int minFlips = 0;
-            while(a != 0 || b != 0 || c != 0){
-                int bit_a = a & 1;
-                int bit_b = b & 1;
-                int bit_c = c & 1;
-    
-                if(bit_c == 1){
-                    // a = 1 or b = 1 or both equals to 1
-                    if(bit_a == 0 && bit_b == 0) minFlips++; 
-                } else {
-                    // a and b must be unset i.e. 0
-                    if((bit_a == 1 && bit_b == 0) || (bit_a == 0 && bit_b == 1)) minFlips++;
-                    else if(bit_a == 1 && bit_b == 1) minFlips += 2;
-                }
-                a >>= 1;
-                b >>= 1;
-                c >>= 1;
+class Solution
+{
+public:
+    int minFlips(int a, int b, int c)
+    {
+        int minFlips = 0;
+        while (a != 0 || b != 0 || c != 0)
+        {
+            int bit_a = a & 1;
+            int bit_b = b & 1;
+            int bit_c = c & 1;
+
+            if (bit_c == 1)
+            {
+                // a = 1 or b = 1 or both equals to 1
+                if (bit_a == 0 && bit_b == 0)
+                    minFlips++;
             }
-    
-            return minFlips;
+            else
+            {
+                // a and b must be unset i.e. 0
+                if ((bit_a == 1 && bit_b == 0) || (bit_a == 0 && bit_b == 1))
+                    minFlips++;
+                else if (bit_a == 1 && bit_b == 1)
+                    minFlips += 2;
+            }
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
         }
-    };
+
+        return minFlips;
+    }
+};
+
+// Approach - 02
+class Solution
+{
+public:
+    int minFlips(int a, int b, int c)
+    {
+        int result1 = (a | b) ^ c;
+        int result2 = (a & b);
+
+        return __builtin_popcount(result1) + __builtin_popcount(result1 & result2);
+    }
+};
