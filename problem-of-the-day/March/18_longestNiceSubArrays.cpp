f812bfc92c2095ca8ea158c949c8c80459c1629c
Rishi Srivastava <rishisrivastava.ju@gmail.com>
2025-03-18 20:12:55 +0530

    Day 18 : Longest Nice Subarrays Approach 02 - sliding window

--- problem-of-the-day/March/18_longestNiceSubArrays.cpp
+++ problem-of-the-day/March/18_longestNiceSubArrays.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
  
+// Approach 01 - TC : O(n^2) & SC : O(1)
 int longestNiceSubarray(vector<int>& nums) {
     int n = nums.size();
     int result = 0;
@@ -12,6 +13,29 @@ int longestNiceSubarray(vector<int>& nums) {
             result = max(result, j-i+1);
         }
     }
+    return result;
+}
+
+// Approach 02 - Sliding Window - TC : O(2*N) & SC : O(1) 
+int longestNiceSubarray(vector<int>& nums) {
+    int n = nums.size();
+
+    int i = 0, j = 0;
+    int mask = 0;
+
+    int result = 1;
+
+    while(j < n){
+        while((mask & nums[j]) != 0){
+            mask ^= nums[i];
+            i++;
+        }
+
+        result = max(result, j - i + 1);
+        mask |= nums[j];
+        j++;
+    }
+
     return result;
 }
 
